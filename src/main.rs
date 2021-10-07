a729f32fc1c73b33ebe5dfc35fd6f6060a51a5a7
Tomasz Sterna <tomasz@sterna.link>
2021-10-07 09:47:21 +0200

    Use ref mut to handle game state

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use duckscript::{
         runtime::Context,
     },
 };
-use std::{cell::RefCell, rc::Rc};
 
 #[derive(Debug)]
 struct GameState {
@@ -14,39 +13,35 @@ struct GameState {
     y: f32,
 }
 
-#[derive(Clone)]
-struct MoveRightCommand {
-    state: Rc<RefCell<GameState>>,
+struct MoveRightCommand<'a> {
+    state: &'a mut GameState,
 }
 
 fn main() {
-    let state = Rc::new(RefCell::new(GameState { x: 0.0, y: 0.0 }));
+    let state = GameState { x: 0.0, y: 0.0 };
 
-    execute_script("moveright", state.clone()).unwrap();
+    execute_script("moveright", &mut state).unwrap();
 
     dbg!(state);
 }
 
-fn execute_script(script: &str, state: Rc<RefCell<GameState>>) -> Result<Context, ScriptError> {
+fn execute_script(script: &str, state: &mut GameState) -> Result<Context, ScriptError> {
     let mut context = Context::new();
     duckscriptsdk::load(&mut context.commands)?;
 
-    context.commands.set(Box::new(MoveRightCommand {
-        state: state.clone(),
-    }))?;
+    context.commands.set(Box::new(MoveRightCommand { state }))?;
 
     runner::run_script(script, context)
 }
 
-impl Command for MoveRightCommand {
+impl Command for MoveRightCommand<'_> {
     fn name(&self) -> String {
         "moveright".to_string()
     }
 
     fn run(&self, _arguments: Vec<String>) -> CommandResult {
-        let mut state = self.state.borrow_mut();
-        state.x += 10.;
-        CommandResult::Continue(Some(state.x.to_string()))
+        self.state.x += 10.;
+        CommandResult::Continue(Some(self.state.x.to_string()))
     }
 
     fn clone_and_box(&self) -> Box<dyn Command> {
